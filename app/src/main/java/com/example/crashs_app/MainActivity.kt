75ee18f5844a266d577fac55c34e5cd022248e6b
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-06-05 17:16:04 -0300

    Adding the dark theme function and setting the value of title text color theme

--- app/src/main/java/com/example/crashs_app/MainActivity.kt
+++ app/src/main/java/com/example/crashs_app/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.crashs_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.crashs_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var checkedThemeGlobal = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.apply {
+            val checked = getBoolean("CHECKED", false)
+            checkedThemeGlobal = checked
+        }
+
+        // Set them on startup
+        setTheme()
     }
+
+    // Get and set theme using set Theme
+    fun theme(view: View?) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        var checked = binding.theme1.isChecked
+        editor.putBoolean("CHECKED", checked).apply()
+        checkedThemeGlobal = checked
+
+        setTheme()
+    }
+
+    // Set theme
+    fun setTheme() {
+        binding.theme1.isChecked = checkedThemeGlobal
+        if (checkedThemeGlobal) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
 }
