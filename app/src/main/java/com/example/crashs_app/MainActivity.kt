de78403c1c62c884cb548dd8986ee684a195d3e3
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-06-06 17:04:00 -0300

    Last date Crash

--- app/src/main/java/com/example/crashs_app/MainActivity.kt
+++ app/src/main/java/com/example/crashs_app/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.crashs_app.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var checkedThemeGlobal = false
     private var crashValue = 0
+    private var formatedDate = " "
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +26,14 @@ class MainActivity : AppCompatActivity() {
         pref.apply {
             val checked = getBoolean("checked", false)
             val getCrashValue = getInt("crashValue", 0)
+            val formatedDate = getString("crashDate", "")
             checkedThemeGlobal = checked
             crashValue = getCrashValue
         }
 
         // Set them on startup
         setTheme()
+        getDate()
         setCrashValue(crashValue)
     }
 
@@ -55,21 +60,40 @@ class MainActivity : AppCompatActivity() {
 
     // Set the crash value
     fun setCrashValue(crashValue: Int) {
-        if (crashValue >= 0) {
             val pref = PreferenceManager.getDefaultSharedPreferences(this)
             val editor = pref.edit()
             editor.putInt("crashValue", crashValue).apply()
             binding.countCrash.text = crashValue.toString()
-        }
     }
 
     fun addCrashValue(view: View?) {
         crashValue++
         setCrashValue(crashValue)
+        setDate()
     }
 
     fun removeCrashValue(view: View?) {
-        crashValue--
-        setCrashValue(crashValue)
+        if (crashValue > 0) {
+            crashValue--
+            setCrashValue(crashValue)
+        }
+    }
+
+    fun getDate() {
+        binding.lastCrashDate.text = formatedDate
+    }
+
+    fun setDate() {
+//        Toast.makeText(applicationContext, "hi", Toast.LENGTH_SHORT).show()
+        var currentDate = System.currentTimeMillis()
+        var formatter = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        var tempFormatedDate = formatter.format(currentDate)
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.putString("crashDate", tempFormatedDate).apply()
+
+        formatedDate = tempFormatedDate
+        getDate()
     }
 }
